00f43125d1036d00edc12289ba563b49f3b2b473
fdrault@enseirb-matmeca.fr <fdrault@enseirb-matmeca.fr>
2017-08-11 17:22:57 +0200

    Add export offline xml

--- src/data_process.cpp
+++ src/data_process.cpp
@@ -1,6 +1,14 @@
 #include "data_process.hpp"
-/*
-void display_all(std::map<long, Anime>* const alist){
-  for (std::map<long, Anime>::iterator it=alist->begin(); it!=alist->end(); ++it)
-    std::cout << it->second;
-}*/
+#include "pugixml.hpp"
+
+void export_all(std::map<long, Anime>* const alist){
+  pugi::xml_document doc;
+  pugi::xml_node header = doc.prepend_child(pugi::node_declaration);
+  header.append_attribute("version") = "1.0";
+  header.append_attribute("encoding") = "UTF-8";
+  for (std::map<long,Anime>::iterator it=alist->begin(); it != alist->end(); ++it){
+    it->second.export_xml(doc);
+  }
+  doc.save_file("export_list.xml");
+}
+
